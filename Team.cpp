acc10a9c88f91deb8a8e98903d3dbd5dcaeb51b7
Ryan Neisius <ryanneisius@gmail.com>
2021-10-18 03:48:41 -0700

    Added TODO for Team private data members.

---
+++ Team.cpp
@@ -0,0 +1,76 @@
+// Team.h  -  Class Implementation
+
+#include "Team.h"
+
+// team constructor that sets private data memebers according to parameters
+Team::Team(string teamName, string stadiumName, int seatingCapacity,
+     string location, string conference, string division,
+     string surfaceType, string stadiumRoofType, int dateOpened)
+{
+    this->teamName = teamName;
+    this->stadiumName = stadiumName;
+    this->seatingCapacity = seatingCapacity;
+    this->location = location;
+    this->conference = conference;
+    this->division = division;
+    this->surfaceType = surfaceType;
+    this->stadiumRoofType = stadiumRoofType;
+    this->dateOpened = dateOpened;
+}
+
+// team destructor
+Team::~Team() {}
+
+// returns the name of the team
+string Team::getTeamName() const
+{
+    return teamName;
+}
+
+// returns the name of the stadium
+string Team::getStadiumName() const
+{
+    return stadiumName;
+}
+
+// returns the seating capacity of the stadium
+int Team::getSeatingCapacity() const
+{
+    return seatingCapacity;
+}
+
+// returns the location of the stadium
+string Team::getLocation() const
+{
+    return location;
+}
+
+// returns the conference of the team
+string Team::getConference() const
+{
+    return conference;
+}
+
+// returns the division of the team
+string Team::getDivision() const
+{
+    return division;
+}
+
+// returns the surface type of the stadium
+string Team::getSurfaceType() const
+{
+    return surfaceType;
+}
+
+// returns the roof type of the stadium
+string Team::getStadiumRoofType() const
+{
+    return stadiumRoofType;
+}
+
+// returns the date the stadium opened
+int Team::getDateOpened() const
+{
+    return dateOpened;
+}
---
+++ Team.h
@@ -0,0 +1,58 @@
+// Team.h  -  Class Specification
+
+#include <string>
+using namespace std;
+
+#ifndef TEAM_H
+#define TEAM_H
+
+class Team
+{
+public:
+    // team constructor that sets private data memebers according to parameters
+    Team(string teamName, string stadiumName, int seatingCapacity,
+         string location, string conference, string division,
+         string surfaceType, string stadiumRoofType, int dateOpened);
+
+    // team destructor
+    ~Team();
+
+    // returns the name of the team
+    string getTeamName() const;
+
+    // returns the name of the stadium
+    string getStadiumName() const;
+
+    // returns the seating capacity of the stadium
+    int getSeatingCapacity() const;
+
+    // returns the location of the stadium
+    string getLocation() const;
+
+    // returns the conference of the team
+    string getConference() const;
+
+    // returns the division of the team
+    string getDivision() const;
+
+    // returns the surface type of the stadium
+    string getSurfaceType() const;
+
+    // returns the roof type of the stadium
+    string getStadiumRoofType() const;
+
+    // returns the date the stadium opened
+    int getDateOpened() const;
+private:
+    string teamName;
+    string stadiumName;
+    int seatingCapacity;
+    string location;
+    string conference;
+    string division;
+    string surfaceType;
+    string stadiumRoofType;
+    int dateOpened;
+};
+
+#endif
